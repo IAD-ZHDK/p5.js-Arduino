4863109cc6d50212c25d664793328108a045d7ca
Luke Franzke <lukefranzke@gmail.com>
2022-04-05 21:17:35 +0200

    Update sketch.js

--- sketch_multinput/sketch.js
+++ sketch_multinput/sketch.js
@@ -1,26 +1,47 @@
+
+let filterArray = []
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   setupSerial();
+  for (let i = 0; i < 10; i++) {
+    filterArray[i] = 1.0
+  }
 }
-
 
 function draw() {
   background(0);
-  var array = SensorData.split(',');
-  var value1 = parseInt(array[0]) / 1024;
-  var value2 = parseInt(array[1]) / 1024;
-
-  fill(value1*255,150,0)
-  let sinWave = sin(radians(frameCount*value1))*(value2*width);
-  circle(width/2,height/2, sinWave)
+  if (SensorData) {
+    var dataArray = SensorData.split(',');
+    let length = dataArray.length
+    // filter 
+    for (let i = 0; i < length; i++) {
+      let value = parseFloat(dataArray[i]);
+      filterArray[i] = filterArray[i] * 0.95
+      filterArray[i] += value * 0.05
+      value = parseFloat(dataArray[i]);
+    }
+    // draw OUTPUT 
+    let spacing = width / length
+    noFill()
+    stroke(255)
+    strokeWeight(3);
+    translate(spacing / 2, 0)
+    for (let i = 0; i < length; i++) {
+      let value = filterArray[i];
+      circle(spacing * i, height / 2, value)
+    }
+  }
 }
 
-function keyPressed() {
-//    let fs = fullscreen();
- //   fullscreen(!fs);
-  }
 
-  /* full screening will change the size of the canvas */
+
+function keyPressed() {
+  //    let fs = fullscreen();
+  //   fullscreen(!fs);
+}
+
+/* full screening will change the size of the canvas */
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
